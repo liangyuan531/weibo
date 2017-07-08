614b5af9ad46fd222d928decf39df7da6c4406ae
yuan <yuanliang123@hotmail.com>
2017-07-08 11:54:05 +0800

    Delegate and datasource

--- source/Weibo/Weibo/Classes/View/Main/WBBaseController.swift
+++ source/Weibo/Weibo/Classes/View/Main/WBBaseController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+//swift 有多继承， OC 没有，使用协议代替
+//class WBBaseController: UIViewController , UITableViewDataSource, UITableViewDelegate
 
-class WBBaseController: UIViewController {
+class WBBaseController: UIViewController{
     var tableView: UITableView?
     
     lazy var navigationBar = UINavigationBar(frame: CGRect(x: 0,y:0, width: UIScreen.cz_screenWidth(), height: 64))
@@ -37,6 +39,9 @@ extension WBBaseController{
     private func setupTableView(){
         tableView = UITableView(frame: view.bounds, style: .plain)
         view.insertSubview(tableView!, belowSubview: navigationBar)
+        //set data source and delegate, in oder to let sub-class implement
+        tableView?.dataSource = self
+        tableView?.delegate = self
     }
     
     private func setupNavigationBar(){
@@ -45,5 +50,16 @@ extension WBBaseController{
         navigationBar.items = [navItem]
         navigationBar.barTintColor = UIColor.cz_color(withHex: 0xF6F6F6)
         navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.darkGray]
+    }
+}
+
+extension WBBaseController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    //基类只是准备方法，子类负责具体的实现
+    //子类的数据源方法不需要supper
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
